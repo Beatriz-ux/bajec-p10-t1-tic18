b40469a297a7cbb6e781769b27caf1e3374d3fc8
Beatriz-ux <beatrizpereira.ld@gmail.com>
2023-09-11 11:56:47 -0300

    feat: inicialiazando a questao

---
+++ src/exercicio.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main(void){
+    vector <string> nomes;
+    vector <float> notas1;
+    vector <float> notas2;
+
+    int N=2;
+
+    for (int i=0; i<N*2,i++){
+        string nome;
+        float n1, n2;
+    }
+    
+
+
+}
