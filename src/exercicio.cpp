03e83e548197712b51ab47fb1aa57578a989f6ca
Beatriz-ux <beatrizpereira.ld@gmail.com>
2023-09-27 00:27:23 -0300

    Feat: etapa 1 , com verificação de notas negativas

--- src/exercicio.cpp
+++ src/exercicio.cpp
@@ -1,18 +1,46 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-int main(void){
-    vector <string> nomes;
-    vector <float> notas1;
-    vector <float> notas2;
+int main(void)
+{
+    vector<string> nomes;
+    vector<float> notas1;
+    vector<float> notas2;
 
-    int N=2;
+    int N;
+    cout << "Digite a quantidade de alunos: ";
+    cin >> N;
+    cin.ignore();
 
-    for (int i=0; i<N*2,i++){
+    for (int i = 0; i < N; i++)
+    {
         string nome;
-        float n1, n2;
+        float nota1, nota2;
+        cout << "Digite o nome do aluno [" << i + 1 << "]: ";
+        getline(cin, nome);
+        do
+        {
+            cout << "Digite a primeira nota: ";
+            cin >> nota1;
+            if (nota1 < 0)
+            {
+                cout << "Nota invalida!" << endl;
+            }
+        } while (nota1 < 0);
+        do
+        {
+            cout << "Digite a segunda nota: ";
+            cin >> nota2;
+            if (nota2 < 0)
+            {
+                cout << "Nota invalida!" << endl;
+            }
+        } while (nota2 < 0);
+
+        cin.ignore();
+        nomes.push_back(nome);
+        notas1.push_back(nota1);
+        notas2.push_back(nota2);
+        cout << endl;
     }
-    
-
-
 }
